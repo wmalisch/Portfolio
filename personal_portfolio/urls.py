0abb5c47508f1ace1d9dd5fbb7a0a2b6d2b41804
Will Malisch <malisch.will@gmail.com>
2020-05-15 09:36:11 -0400

    Started project from scratch because some errors in last attempt had me changing too many files names, and got everything disorganized

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home)
 
 ]
 
