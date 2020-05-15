e9905c273297db39277d3e44af6614624436fca6
Will Malisch <malisch.will@gmail.com>
2020-05-15 12:44:32 -0400

    Set up some more functionality and added some files

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField(auto_now=False)
+    tweet = models.CharField(max_length=320)
+    url = models.URLField(blank=True)
+
