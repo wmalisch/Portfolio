08a7a795311a83ded39acc0b5c9e79a7d35ef3d6
Will Malisch <malisch.will@gmail.com>
2020-05-19 10:27:36 -0400

    Fixed some bugs with the URL names, application is now fully functional

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -6,7 +6,7 @@ from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home, name='home'),
     path('blog/', include('blog.urls')),
 ]
 
